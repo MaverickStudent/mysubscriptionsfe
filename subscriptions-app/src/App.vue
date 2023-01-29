ec37cfe1072c3ee34504a9e1cdbe08ecffbdc626
MaverickStudent <miguelrocha@unomaha.edu>
2023-01-29 12:38:52 -0700

    Update App.vue

--- subscriptions-app/src/App.vue
+++ subscriptions-app/src/App.vue
@@ -19,48 +19,49 @@
     </main>
   </div>
 </template>
-<style lang="scss"> @import url('https://fonts.googleapis.com/css2?family=Fira+Sans:wght@500&display=swap');
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: 'Fira Sans', san-serif;
+<style lang="scss">
+@import
+url('https://fonts.googleapis.com/css2?family=Fira+Sans:wght@500&display=swap');
 
-    &::selection {
-      background: transparentize(#42B883, 0.5);
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', san-serif;
+
+  &::selection {
+    background: transparentize(#42B883, 0.5);
+  }
+}
+
+body {
+  background-color: #35495B;
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  display: flex;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #2C3D4E;
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.1);
+
+  h1 {
+    color: #FFF;
+    font-size: 28px;
+
+    span {
+      color: #42B883;
     }
   }
-
-  body {
-    background-color: #35495B;
-  }
-
-  a {
-    text-decoration: none;
-  }
-
-  header {
-    display: flex;
-    justify-content: center;
-    padding: 10px 16px;
-    background-color: #2C3D4E;
-    box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.1);
-
-    h1 {
-      color: #FFF;
-      font-size: 28px;
-
-      span {
-        color: #42B883;
-      }
-    }
-  }
-
-  h3 {
-    padding-top: 10px;
-  }
-
-  .modify {
-    padding: 10px;
-  }
+}
+h3 {
+  padding-top: 10px;
+}
+.modify {
+  padding: 10px;
+}
 </style>
